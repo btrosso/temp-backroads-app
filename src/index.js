fbaa3833f8b41a1b0c4fb5ff2a483b36dfcb7a5e
brosso <brosso@analyticalai.com>
2025-03-01 22:48:41 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// this is the file where we inject our javascript logic into the index.html (the public one)
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
