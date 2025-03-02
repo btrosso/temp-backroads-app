6d6e9b563e0463e5dcb324400c427fa01f5a8288
brosso <brosso@analyticalai.com>
2025-03-01 22:58:30 -0600

    small change to test netlify continuous deploy

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,7 @@ const Hero = () => {
         <h1>backroads app</h1>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
-          explicabo debitis est autem dicta.
+          explicabo debitis est autem dicta!
         </p>
         <a href="#tours" className="btn hero-btn">
           explore tours
